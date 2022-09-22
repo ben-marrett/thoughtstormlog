c62abdcc6ba18b0146155e16347176340ebcf3f3
ben-marrett <benteiko@gmail.com>
2022-09-22 14:11:45 +1200

    rendering search results very roughly for now.

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,6 +3,6 @@ import { searchLogs } from "../../lib/redis";
 export default async function handler(req, res) {
   const q = req.query.q;
   const logs = await searchLogs(q);
-  res.status(200).json({logs})
+  res.status(200).json({ logs });
 
 }
